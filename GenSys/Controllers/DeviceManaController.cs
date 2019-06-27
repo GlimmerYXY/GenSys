11a2e8ed6f7a20c6b2265790ab09af7e3497610e
Small ASUS <glimmer_ye@163.com>
2019-06-27 17:16:43 +0800

    设备管理 设备类型下拉框生成的选项被自动隐藏

--- GenSys/Controllers/DeviceManaController.cs
+++ GenSys/Controllers/DeviceManaController.cs
@@ -14,38 +14,43 @@ namespace AspDotNetMVCBootstrapTable.Controllers
         // GET: DeviceManaController
         public ActionResult Index()
         {
-            List<SelectListItem> siteList = new SelectList(db.site, "name", "name").ToList();
-            siteList.Insert(0, new SelectListItem { Text = "请选择/输入站点", Value = "yxy", Selected = true, Disabled = true });
+            var site = from device in db.device
+                           select device.site;
+            List<SelectListItem> siteList = new SelectList(site.Distinct()).ToList();
+            siteList.Insert(0, new SelectListItem { Text = "请选择/输入站点", Selected = true, Disabled = true });
             ViewData["siteList"] = siteList;
 
             var dev_type = from device in db.device
                            select device.dev_type;
-            List<SelectListItem> dev_typeList = new SelectList(dev_type, "dev_type", "dev_type").ToList();
-            dev_typeList.Insert(0, new SelectListItem { Text = "请选择设备类型", Value = "yxy", Selected = true, Disabled = true });
+            List<SelectListItem> dev_typeList = new SelectList(dev_type.Distinct()).ToList();
+            dev_typeList.Insert(0, new SelectListItem { Text = "请选择/输入设备类型",  Selected = true, Disabled = true });
             ViewData["dev_typeList"] = dev_typeList;
 
             var dev_model = from device in db.device
-                           select device.dev_model;
-            List<SelectListItem> dev_modelList = new SelectList(dev_model, "dev_model", "dev_model").ToList();
-            dev_modelList.Insert(0, new SelectListItem { Text = "请选择设备类型", Value = "yxy", Selected = true, Disabled = true });
+                            select device.dev_model;
+            List<SelectListItem> dev_modelList = new SelectList(dev_model.Distinct()).ToList();
+            dev_modelList.Insert(0, new SelectListItem { Text = "请选择/输入设备型号", Selected = true, Disabled = true });
             ViewData["dev_modelList"] = dev_modelList;
-
+            
+            ViewData["device"] = db.device.ToList();
             return View();
         }
-        
+
         // POST: /DeviceMana/AddDevice
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "id,type,model")] device device)
+        public ActionResult AddDevice([Bind(Include = "site, dev_type, dev_model, alias, ip, media_port, username, password")] device device)
         {
             if (ModelState.IsValid)
             {
+                int count = db.device.Count();
+                device.id = count + 1;
                 db.device.Add(device);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            return View(device);
+            
+            return View();
         }
         
     }

546498500937be68501962e945037dae4338a170
Arthur <Arthur@DESKTOP-29VH24R>
2019-07-07 15:19:49 +0800

    model/gensysmodel

--- GenSys/Controllers/UserManaController.cs
+++ GenSys/Controllers/UserManaController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using AspDotNetMVCBootstrapTable.Models;
-using GenSys.MySQL;
+using GenSys.Models;
+
 
 namespace AspDotNetMVCBootstrapTable.Controllers
 {

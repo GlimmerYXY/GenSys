7b27ca2dc8047bde68aed5ec3c6b9c8e91a0d073
Small ASUS <glimmer_ye@163.com>
2019-08-26 10:30:55 +0800

    linkage ui

--- GenSys/Controllers/LinkageConfigController.cs
+++ GenSys/Controllers/LinkageConfigController.cs
@@ -11,7 +11,33 @@ namespace GenSys.Controllers
         // GET: LinkageConfig
         public ActionResult Index()
         {
+            //var site = from device in db.device
+            //           select device.site;
+            //List<SelectListItem> siteList = new SelectList(site.Distinct()).ToList();
+            List<SelectListItem> siteList = new List<SelectListItem>();
+            //siteList.Insert(0, new SelectListItem { Text = "请选择", Selected = true, Disabled = true });
+            siteList.Insert(0, new SelectListItem { Text = "开门" });
+            siteList.Insert(1, new SelectListItem { Text = "关门" });
+            ViewData["event"] = siteList;
+
             return View();
+        }
+
+        public ActionResult Generate(FormCollection collection)
+        {
+            var Mon = collection["Mon"];
+            var Tues = collection["Tues"];
+            var Wed = collection["Wed"];
+            var Thur = collection["Thur"];
+            var Fri = collection["Fri"];
+            var Sat = collection["Sat"];
+            var Sun = collection["Sun"];
+
+            //var Mon = collection["Mon"];
+            //var Mon = collection["Mon"];
+            //var Mon = collection["Mon"];
+            
+            return RedirectToAction("Index");
         }
     }
 }
